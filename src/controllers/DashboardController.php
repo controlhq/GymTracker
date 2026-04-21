3a56e74a4248cfe12adadc6fe060908c5863a4ef
Arkadiusz Domżał <paniqq27@gmail.com>
2026-04-21 17:16:26 +0200

    dashboard: clean up DashboardController and render display_name

--- src/controllers/DashboardController.php
+++ src/controllers/DashboardController.php
@@ -1,17 +1,14 @@
 <?php
 
 require_once 'AppController.php';
-require_once __DIR__.'/../repositories/UsersRepository.php';
 
 class DashboardController extends AppController {
 
     public function index() {
         $this->requireLogin();
 
-        $title = "INDEX";
-        $usersRepository = new UsersRepository();
-        $users = $usersRepository->getUsers();
-
-        return $this->render("index", ["title" => $title, "users" => $users]);
+        return $this->render('dashboard', [
+            'displayName' => $_SESSION['user_display_name']
+        ]);
     }
 }
