d51039381af08b33e2ef3da2dc4c281b75f8f1ce
Arkadiusz Domżał <paniqq27@gmail.com>
2026-05-26 21:55:02 +0200

    add csrf token protection to login and register forms

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -21,6 +21,20 @@ class AppController {
         }
     }
 
+    protected function generateCsrfToken(): string
+    {
+        if (empty($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
+    }
+
+    protected function validateCsrfToken(): bool
+    {
+        $token = $_POST['csrf_token'] ?? '';
+        return !empty($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
+    }
+
     protected function render(string $template = null, array $variables = [])
     {
         $templatePath = 'public/views/'. $template.'.html';
