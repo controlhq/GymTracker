0db72ec7c0b5f1101c6816d3b8a8c1f79e6d2aeb
Arkadiusz Domżał <paniqq27@gmail.com>
2026-05-26 21:53:50 +0200

    harden session cookie: set httponly and samesite=lax flags

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path'     => '/',
+    'httponly' => true,
+    'samesite' => 'Lax',
+]);
+
 session_start();
 
 require_once 'Routing.php';
